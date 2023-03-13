3ef13adc15721f3a8a86e2904cd282c85514df0b
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-13 22:30:09 +0000

    fixed footer to stay at bottom

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,25 +4,24 @@ import { AppBar, Toolbar, Typography, makeStyles} from "@material-ui/core";
 const useStyles = makeStyles(() => ({  
   bottom: {
     width: '100%',
-    position: 'fixed',
+    height: '8%',
+    position: 'sticky',
     bottom: 0,
   },
 }));
 
-
-const Footer
-
-
-const Footer() {}
+function Footer() {
 
   const { bottom } = useStyles();
+
+  return (
 
         <AppBar className={bottom}>
             <Toolbar style={{ justifyContent: "center" }}>
                 <Typography variant="caption">©2020</Typography>
              </Toolbar>
          </AppBar>
+)};
 
-
-  
 export default Footer;
+
