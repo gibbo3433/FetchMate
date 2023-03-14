d0aca79990d018895af6534b6c1d6002b8cbf36d
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-14 21:39:15 +0000

    updated header - need to change links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,89 +1,58 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button, makeStyles } from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
+// import { Link as RouterLink } from "react-router-dom";
 
-const useStyles = makeStyles(() => ({  
-  logo: {
-    fontFamily: "Work Sans, sans-serif",
-    fontWeight: 600,
-    color: "#FFFEFE",
-    textAlign: "left",
-  },
-  tabButtons: {
-    fontFamily: "Open Sans, sans-serif",
-    fontWeight: 700,
-    size: "18px",
-    marginLeft: "38px",
-  },
-  toolbar: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  header: {
-    backgroundColor: "#400CCC",
-    paddingRight: "79px",
-    paddingLeft: "118px",
-  },
-}));
-
-const headersData = [
-  {
-    label: "Dashboard",
-    href: "/dashboard",
-  },
-  {
-    label: "Profile",
-    href: "/profile",
-  },
-  {
-    label: "Log Out",
-    href: "/logout",
-  },
-  {
-    label: "Login",
-    href: "/login",
-  },  {
-    label: "About uS",
-    href: "/about",
-  },
-];
-
-function Header() {
-
-  const { logo, tabButtons, toolbar } = useStyles();
-
-  const displayDesktop = () => {
-    return <Toolbar className={toolbar}>{Logo}<div>{getMenuButtons()}</div></Toolbar>;
-  };
-
-  const getMenuButtons = () => {
-  return headersData.map(({ label, href }) => {
-    return (
-      <Button className={tabButtons}
-        {...{
-          key: label,
-          color: "inherit",
-          to: href,
-          component: RouterLink,
-        }}
-      >
-        {label}
-      </Button>
-    );
-  });
- };
-
-  const Logo = (
-    <Typography variant="h6" component="h1" className={logo}>
-      Fetch-Mate
-    </Typography>
-  );
-
+export default function Header() {
   return (
-    <header >
-      <AppBar>{displayDesktop()}</AppBar>
+    <header class="p-4 dark:bg-gray-800 dark:text-gray-100">
+      <div class="container flex justify-between h-16 mx-auto">                 
+        <ul class="items-stretch hidden space-x-3 lg:flex">
+          <li class="flex">
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/"
+              class="flex items-center px-6 -mb-1 border-b-2 dark:border-transparent dark:text-violet-100 dark:border-violet-400"
+            >
+              Fetch Mate
+            </a>
+          </li>
+          <li class="flex">
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/"
+              class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+            >
+              Login
+            </a>
+          </li>
+          <li class="flex">
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/"
+              class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+            >
+              Logout
+            </a>
+          </li>
+          <li class="flex">
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/"
+              class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+            >
+              Signup
+            </a>
+          </li>
+          <li class="flex">
+            <a
+              rel="noopener noreferrer"
+              href="https://github.com/"
+              class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+            >
+              Profile
+            </a>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
-
-export default Header;
