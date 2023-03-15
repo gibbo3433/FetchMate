7df9d45321a9ae98f3156801743b183ed25aa81d
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-15 19:10:41 +0000

    product card holding the hero

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,27 +1,48 @@
 import { Link } from 'react-router-dom';
 
-const ProductCard = ( props ) => {
 
-    console.log(props)
-    const {title, description, image, price, _id, onAddToCart} = props;
+const ProductCard = ( props ) => {
 
 
   return (
-    <div className='border p-5 m-2 flex'>
-        <div>
-            <img src={`/assets/${image}`} alt={title} className='product-image p-5' />
-        </div>
-        <div className='product-details'>
-            <Link to={`/product/${_id}`}>{title}</Link>
-            <div className='product-description'>
-                <p className='text-sm'>{description}</p>
-            </div>
-            <div className='product-price'>
-                <p className='text-lg mr-5'>£{price}</p>
-                <button className='btn btn-primary' onClick={onAddToCart}>Add to Cart</button>
-            </div>
-        </div>
-    </div>
+    <section class="dark:dark:bg-gray-800 dark:dark:text-gray-100">
+	<div class="container flex flex-col-reverse mx-auto lg:flex-row">
+		<div class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:dark:bg-violet-400 dark:dark:text-gray-900">
+			<div class="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div class="space-y-2">
+					<p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+					<p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+				</div>
+			</div>
+			<div class="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div class="space-y-2">
+					<p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+					<p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+				</div>
+			</div>
+			<div class="flex space-x-2 sm:space-x-4">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+				</svg>
+				<div class="space-y-2">
+					<p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+					<p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+				</div>
+			</div>
+		</div>
+		<div class="lg:w-1/2 xl:w-3/5 dark:dark:bg-gray-800">
+			<div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
+				<img src="https://source.unsplash.com/640x480/" alt="" class="rounded-lg shadow-lg dark:dark:bg-gray-500 aspect-video sm:min-h-96"></img>
+			</div>
+		</div>
+	</div>
+</section>
  )
 }
 
