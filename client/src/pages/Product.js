a630c24f21cd5c09df53d72dc5d4d2f480682eec
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-15 19:11:58 +0000

    product holding the hero

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -6,44 +6,15 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_PRODUCT } from '../utils/queries';
 
+
 // Shopping Cart
 import { useCart } from '../context/CartContext'
+import Hero from '../components/Hero';
 
 const Product = () => {
 
-  const { onAddToCart } = useCart()
-
-  const params = useParams();
-  const { productId } = useParams();
-
-  console.log('params: ', params)
-
-  const { loading, data } = useQuery(QUERY_SINGLE_PRODUCT, {
-    // pass URL parameter
-    variables: { productId: productId },
-  });
-
-  const product = data?.product || {};
-  const productTitle = loading ? 'Loading Product...' : data?.product.title;
-  console.log(`Product: products = ${data}`)
-
   return (
-    <>
-      <div className='p-5 m-2 border w-75'>
-        <h1>{productTitle}</h1>
-        <div className='section-title'>
-
-          <ProductCard key={product.title} {...product} onAddToCart={() => onAddToCart(product)} />
-
-        </div>
-      </div>
-      <div className='p-5 m-2 border w-25'>
-        <div className='section-title'>
-          Browse the Shop
-        </div>
-        <CategoriesLinks />
-      </div>
-    </>
+    <Hero />
   )
 }
 
--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -1,49 +1,13 @@
 // UI Components
-import ProductCard from '../components/ProductCard'
-import CategoriesLinks from '../components/CategoriesLinks'
+import Hero from '../components/Hero';
 
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_SINGLE_CATEGORY } from '../utils/queries';
 
-// Shopping Cart
-import { useCart } from '../context/CartContext'
 
 const ProductList = () => {
-  
-  const { onAddToCart } = useCart()
-
-  const { category } = useParams();
-
-  console.log(`ProductList: category = ${category}`)
-
-  const { loading, data } = useQuery(QUERY_SINGLE_CATEGORY, {
-    // pass URL parameter
-    variables: { name: category },
-  });
-
-  const products = data?.category.products || [];
-  const categoryName = data?.category.name || 'Loading Products...';
-  console.log(`ProductList: products = ${products}`)
 
   return (
-    <>
-      <div className='w-75 border m-2 p-5'>
-        <h1>All {categoryName}</h1>
-        <div className='section-title'>
-          {products.map(product => (
-            <ProductCard key={product.title} {...product} onAddToCart={()=>onAddToCart(product)} />
-          ))}
-        </div>
-      </div>  
-      <div className='w-25 border m-2 p-5'>
-        <div className='section-title'>
-            Browse the Shop
-        </div>
-        <CategoriesLinks />
-      </div>
-    </>
-  )
+     <Hero />
+    )
 }
 
 export default ProductList
