d7192933de5ef8ca3f5ef50294dc13a88c4e0518
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-16 19:47:45 +0000

    added home router links

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,36 +1,31 @@
 // UI Components
-import UserCard from '../components/UserCard'
-import Hero from '../components/Hero';
+import UserCard from "../components/UserCard";
+import Hero from "../components/Hero";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_LOCAL_USERS } from '../utils/queries';
-
+import { useQuery } from "@apollo/client";
+import { QUERY_LOCAL_USERS } from "../utils/queries";
 
 const Home = () => {
-
   const { loading, data } = useQuery(QUERY_LOCAL_USERS);
   const users = data?.users || [];
 
-  console.log(users)
-
+  console.log(users);
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      <div>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <div style={{ width: "100%" }}>
         <Hero />
       </div>
   
-      
-  
-      <div className='p-5 m-2 border' style={{ width: '100%', order: 2 }}>
-        <div className='section-title'>
-          {users.map(user => (
+      <div className="p-5 m-2 border" style={{ width: "100%", order: 2 }}>
+        <div className="section-title">
+          {users.map((user) => (
             <UserCard key={user.username} {...user} />
           ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
