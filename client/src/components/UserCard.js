2239b67f7ae6d4a8508a6445545d7a6b6631f113
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-17 22:20:41 +0000

    edited home user cards

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -1,19 +1,23 @@
 import { Link } from 'react-router-dom';
 
-
 const UserCard = (props) => {
 
-    const { _id, username, email } = props;
+    const { _id, username, userAge, location, userDogName, userDogBreed, userDogBio } = props;
 
     return (
         <div className='flex p-5 m-2 border'>
             <div>
-                <img src={`https://i.pravatar.cc/50?u=${username}`} alt={username} className='p-5 product-image' />
+                <img src={`https://i.pravatar.cc/50?u=${username}`} alt={username} className='p-2 rounded-full' width={150} height={150} />
             </div>
             <div className='dog-details'>
                 <Link to={`/dog/${_id}`}>{username}</Link>
                 <div className='dog-description'>
-                    <p className='text-sm'>{email}</p>
+                    <p className='text-sm'>Age: {userAge}</p>
+                    <p className='text-sm'>Location: {location}</p>
+                    {/* img link for dogs</p> */}
+                    <p className='text-sm'>Dog Name: {userDogName}</p>
+                    <p className='text-sm'>Breed: {userDogBreed}</p>
+                    <p className='text-sm'>{userDogBio}</p>
                 </div>
             </div>
         </div>
