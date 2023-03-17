89459f621f107fe47092281f234c23437321f238
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-03-17 20:31:33 +0000

    routerlink to sign up page

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Link, Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Hero () {
     return (
-        <section class="dark:dark:bg-gray-800 dark:dark:text-gray-100">
-            <div class="container flex flex-col-reverse mx-auto lg:flex-row">
-                <div class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:dark:bg-violet-400 dark:dark:text-gray-900">
-                    <div class="flex space-x-2 sm:space-x-4">
-                        <div class="text-wrapper space-y-2">
-    <p class="text-3xl font-bold leading-snug">Fetch mate</p>
-    <p class="text-xl leading-snug">Find local dog lovers in your area!</p>
-    <button class="px-4 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Signup to get started!</button>
-</div>
+        <section className="dark:dark:bg-gray-800 dark:dark:text-gray-100">
+            <div className="container flex flex-col-reverse mx-auto lg:flex-row">
+                <div className="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:dark:bg-violet-400 dark:dark:text-gray-900">
+                    <div className="flex space-x-2 sm:space-x-4">
+                        
+                        <div className="text-wrapper space-y-2">
+                            <p className="text-3xl font-bold leading-snug">Fetch mate</p>
+                            <p className="text-xl leading-snug">Find local dog lovers in your area!</p>
+                            <Link to="/signup" className="px-4 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Signup to get started!</Link>
+                        </div>
                     </div>
                 </div>
-                <div class="lg:w-1/2 xl:w-3/5 dark:dark:bg-gray-800">
-                    <div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
+                <div className="lg:w-1/2 xl:w-3/5 dark:dark:bg-gray-800">
+                    <div className="flex items-center justify-center p-4 md:p-8 lg:p-12">
+
                         <img src="https://publish.purewow.net/wp-content/uploads/sites/2/2022/08/dog-breed-groups.jpg?resize=1200%2C628" alt="dogs" class="rounded-lg shadow-lg dark:dark:bg-gray-500 w-full"></img>
                     </div>
                 </div>
