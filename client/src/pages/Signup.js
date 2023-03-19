bda9d4b70f1d557704f21a5d32a1d5c7ab6b7b8b
Maartje Oostvogels <moostvogels@hotmail.com>
2023-03-19 13:24:46 +0000

    Fixed error causing sign up sheet to not load up, added styling to form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
+import { UserGender, WalkTimes, DogBreeds, DogAges } from "../data/data";
 
 import Auth from "../utils/auth";
 
@@ -15,12 +16,14 @@ const Signup = () => {
     userGender: "",
     userAge: "",
     location: "",
-    walkTimes: "",
-    dogBreeds: "",
-    userDogAge: "",
+    userBio: "",
+    userWalkTimes: "",
     userDogName: "",
+    userDogBreed: "",
+    userDogAge: "",
+    userDogBio: "",
   });
-
+  const [validationErrors, setValidationErrors] = useState([]);
   const [addUser, { error, data }] = useMutation(ADD_USER);
 
   const handleChange = (event) => {
@@ -32,9 +35,88 @@ const Signup = () => {
     });
   };
 
+  const handleGenderChange = (data) => {
+    const { label, value } = data;
+    setFormState({
+      ...formState,
+      userGender: value,
+    });
+  };
+
+  const handleDogAgeChange = (data) => {
+    const { label, value } = data;
+    setFormState({
+      ...formState,
+      userDogAge: value,
+    });
+  };
+
+  const handleWalkTimeChange = (data) => {
+    const { label, value } = data;
+    setFormState({
+      ...formState,
+      walkTime: value,
+    });
+  };
+
+  const handleDogBreedChange = (data) => {
+    const { label, value } = data;
+    setFormState({
+      ...formState,
+      dogBreed: value,
+    });
+  };
+
+  const validateSubmission = () => {
+    const errors = [];
+    if (formState.username.length < 1) {
+      errors.push("Please enter a username.");
+    }
+    if (formState.email.length < 1) {
+      errors.push("Please enter an email address.");
+    }
+    if (formState.password.length < 1) {
+      errors.push("Please enter a password.");
+    }
+    if (formState.userGender.length < 1) {
+      errors.push("You must specify your Gender.");
+    }
+    if (formState.userAge.length < 1) {
+      errors.push("You must specify your Age.");
+    }
+    if (formState.location.length < 1) {
+      errors.push("You must specify your Location.");
+    }
+    if (formState.userBio.length < 1) {
+      errors.push("You must enter a Bio.");
+    }
+    if (formState.userWalkTimes.length < 1) {
+      errors.push("You must enter your preferred Walk Times.");
+    }
+    if (formState.userDogName.length < 1) {
+      errors.push("You must enter your Dog's Name.");
+    }
+    if (formState.userDogBreed.length < 1) {
+      errors.push("You must enter your Dog's Breed.");
+    }
+    if (formState.userDogAge.length < 1) {
+      errors.push("You must enter your Dog's Age.");
+    }
+    if (formState.userDogBio.length < 1) {
+      errors.push("You must enter your Dog's Bio.");
+    }
+    return errors;
+  };
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    console.log(formState);
+
+    // Check this is a valid submission first
+    const errors = validateSubmission();
+    if (errors.length > 0) {
+      setValidationErrors(errors);
+      return;
+    }
 
     try {
       const { data } = await addUser({
@@ -42,170 +124,142 @@ const Signup = () => {
       });
 
       Auth.login(data.addUser.token);
+      setValidationErrors([]);
     } catch (e) {
       console.error(e);
     }
   };
 
-  const userGender = [
-    { value: "Male", label: "Male" },
-    { value: "Female", label: "Female" },
-    { value: "Other", label: "Other" },
-  ];
-
-  const walkTimes = [
-    { value: "0-30 minutes", label: "0-30 minutes" },
-    { value: "30-60 minutes", label: "30-60 minutes" },
-    { value: "1 hour - 2 hours", label: "1 hour - 2 hours" },
-    { value: "2 hours +", label: "2 hours +" },
-  ];
-
-  const dogBreeds = [
-    { value: "Airedale Terrier", label: "Airedale Terrier" },
-    { value: "Bulldog", label: "Bulldog" },
-    { value: "Chihuahua", label: "Chihuahua" },
-    { value: "Dachshund", label: "Dachshund" },
-    { value: "English Springer Spaniel", label: "English Springer Spaniel" },
-    { value: "French Bulldog", label: "French Bulldog" },
-    { value: "German Shepherd", label: "German Shepherd" },
-    { value: "Golden Retriever", label: "Golden Retriever" },
-    { value: "Labrador Retriever", label: "Labrador Retriever" },
-    { value: "Poodle", label: "Poodle" },
-    { value: "Pomeranian", label: "Pomeranian" },
-    { value: "Rottweiler", label: "Rottweiler" },
-    { value: "Siberian Husky", label: "Siberian Husky" },
-    { value: "Shih Tzu", label: "Shih Tzu" },
-    {
-      value: "Staffordshire Bull Terrier",
-      label: "Staffordshire Bull Terrier",
-    },
-    { value: "St. Bernard", label: "St. Bernard" },
-    { value: "Toy Poodle", label: "Toy Poodle" },
-    { value: "Yorkshire Terrier", label: "Yorkshire Terrier" },
-    {
-      value: "West Highland White Terrier",
-      label: "West Highland White Terrier",
-    },
-    { value: "Weimaraner", label: "Weimaraner" },
-  ];
-
   return (
-    <main style={{ display: "flex", flexDirection: "column" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div
-            style={{ display: "flex", flexDirection: "column" }}
-            className="card-body"
-          >
-            {data ? (
-              <p>
-                Success! You may now head{" "}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Username"
-                  name="username"
-                  type="text"
-                  value={formState.username}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="E-mail"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <Select
-                  className="form-input"
-                  placeholder="Select Gender"
-                  name="userGender"
-                  options={userGender}
-                  value={formState.userGender}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Enter Age Here"
-                  name="userAge"
-                  type="text"
-                  value={formState.userAge}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Enter Location Here"
-                  name="location"
-                  type="text"
-                  value={formState.location}
-                  onChange={handleChange}
-                />
-                <Select
-                  className="form-input"
-                  placeholder="Select Walk Time"
-                  name="walkTimes"
-                  type=""
-                  value={formState.walkTimes}
-                  onChange={handleChange}
-                  options={walkTimes}
-                />
-                <Select
-                  className="form-input"
-                  placeholder="Select Dog Breed"
-                  name="dogBreed"
-                  value={formState.dogBreeds}
-                  onChange={handleChange}
-                  options={dogBreeds}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Enter Dog Age Here"
-                  name="userDogAge"
-                  type="number"
-                  value={formState.userDogAge}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Enter Dog Name Here"
-                  name="userDogName"
-                  type="text"
-                  value={formState.userDogName}
-                  onChange={handleChange}
-                />
-
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: "pointer" }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+    <>
+      <div className="items-center mx-auto w-96 form-container justify-items-center">
+        <h4 className="form-heading">Sign Up</h4>
+        {data ? (
+          <p>
+            Success! You may now head <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <div className="">
+            <form onSubmit={handleFormSubmit} className="flex flex-col">
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Your username"
+                name="username"
+                type="text"
+                value={formState.name}
+                onChange={handleChange}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <Select
+                className="mt-2 form-select"
+                placeholder="Select Gender"
+                name="userGender"
+                setValue={formState.userGender}
+                onChange={handleGenderChange}
+                options={UserGender}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Enter your age"
+                name="userAge"
+                type="text"
+                value={formState.userAge}
+                onChange={handleChange}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Enter your Location"
+                name="location"
+                type="text"
+                value={formState.location}
+                onChange={handleChange}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Bio"
+                name="userBio"
+                type="text"
+                value={formState.userBio}
+                onChange={handleChange}
+              />
+              <Select
+                className="mt-2 form-select"
+                placeholder="Typical Walk Time"
+                name="userWalkTimes"
+                setValue={formState.userWalkTimes}
+                onChange={handleWalkTimeChange}
+                options={WalkTimes}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Enter your Dog's Name"
+                name="userDogName"
+                type="number"
+                value={formState.userDogName}
+                onChange={handleChange}
+              />
+              <Select
+                className="mt-2 form-select"
+                placeholder="Select Breed"
+                name="userDogBreed"
+                setValue={formState.userDogBreed}
+                onChange={handleDogBreedChange}
+                options={DogBreeds}
+              />
+              <Select
+                className="mt-2 form-select"
+                placeholder="Enter your Dog's Age"
+                name="userDogAge"
+                setValue={formState.userDogAge}
+                onChange={handleDogAgeChange}
+                options={DogAges}
+              />
+              <input
+                className="w-full px-3 py-2 mt-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                placeholder="Enter Dog Bio"
+                name="userDogBio"
+                type="text"
+                value={formState.userDogBio}
+                onChange={handleChange}
+              />
+              <button
+                className="px-4 py-2 my-2 text-teal-500 lowercase transition duration-300 ease-in-out border border-teal-500 rounded-md hover:bg-teal-500 hover:text-white animate-pulse"
+                style={{ cursor: "pointer" }}
+                type="submit"
+              >
+                Sign Up
+              </button>
+            </form>
           </div>
-        </div>
+        )}
+
+        {error && (
+          <div className="p-3 my-3 text-red bg-danger">{error.message}</div>
+        )}
+        {validationErrors.length > 0 && (
+          <div className="p-3 my-3 text-red bg-danger">
+            {validationErrors.map((err) => (
+              <p key={err}>{err}</p>
+            ))}
+          </div>
+        )}
       </div>
-    </main>
+    </>
   );
 };
 
